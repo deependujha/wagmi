925158173a39a51bd74ec321a29be4c5e020dec8
deependujha <deependujha21@gmail.com>
2023-02-03 01:57:48 +0530

    rainbow kit done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { goerli, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+	[polygonMumbai, goerli],
+	[publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+	appName: 'Wagmi-RainbowKit',
+	chains,
+});
+
+const wagmiClient = createClient({
+	autoConnect: false,
+	connectors,
+	provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
 }
